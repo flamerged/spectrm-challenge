d45cf6054c7c6e0582d4e440220a1118d9ab2d53
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-02-08 12:37:57 +0100

    fixed babel issues

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const port = process.env.PORT || 4000;
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to Node.js & Express" });
+});
+
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
 });
