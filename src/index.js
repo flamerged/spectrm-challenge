b2462c97eff222402dcc8a917643f97ca260b33a
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-02-08 14:01:41 +0100

    saving to create template

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to Node.js & Express" });
 });
 
+app.get("/message/:id", (req, res) => {
+  console.log(parseInt(req.params.id) + 5);
+  res
+    .status(200)
+    .json({ message: `Yes hello this is message ${req.params.id}` });
+});
+
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
 });
