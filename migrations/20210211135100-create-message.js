1b07c0583f382e0476196cecdd77cf06919cac54
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-02-11 20:17:28 +0100

    added basic crud methods for messages

--- migrations/20210211135100-create-message.js
+++ migrations/20210211135100-create-message.js
@@ -4,7 +4,7 @@ module.exports = {
     await queryInterface.createTable("Messages", {
       id: {
         type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
+        defaultValue: Sequelize.UUIDV4(),
         allowNull: false,
         primaryKey: true,
       },
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
-import bodyParser from "body-parser";
 import { db } from "./config/db";
+import models from "../models/index";
+import { v4 as uuid } from "uuid";
 
 const port = process.env.PORT || 4000;
+const { Message } = models;
 
 db.authenticate()
   .then(() => console.log(`connected to ${process.env.NODE_ENV} db`))
@@ -11,20 +13,87 @@ db.authenticate()
   );
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.urlencoded());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes todo
 
 // Get all messages
 
+app.get("/messages", async (req, res) => {
+  try {
+    const messages = await Message.findAll();
+    res.status(200).send(messages);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ status: "failed", error: `${error}` });
+  }
+});
+
 // Get single message by ID
+
+app.get("/messages/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const message = await Message.findAll({ where: { id } });
+    res.status(200).send(message);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ status: "failed", error: `${error}` });
+  }
+});
 
 // Create a message
 
+app.post("/messages/", async (req, res) => {
+  const { content } = req.body;
+  console.log(req.body);
+  const id = uuid();
+
+  try {
+    const message = await Message.create({ id, content, retrievedCounter: 0 });
+    res.status(201).send(message);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: "failed", error: `${error}` });
+  }
+});
+
 // Update a message by ID
 
+app.put("/messages/:id", async (req, res) => {
+  const changes = req.body;
+  const id = req.params.id;
+
+  try {
+    const message = await Message.update(
+      { ...changes },
+      { where: { id }, returning: true }
+    );
+    res.status(202).send({ status: "success", message: message[1][0] });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: "failed", error: `${error}` });
+  }
+});
+
 // delete a message by ID
+
+app.delete("/messages/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const message = await Message.destroy({ where: { id }, returning: true });
+    if (message === 0) {
+      throw new Error("Message could not be found");
+    }
+    res.status(202).send({ status: "success" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ status: "failed", error: `${error}` });
+  }
+});
 
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
