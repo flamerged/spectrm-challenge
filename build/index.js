a094204d2b581a9020d98d2d146571a6bc798612
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-02-11 23:17:32 +0100

    sequelize production fix

--- build/index.js
+++ build/index.js
@@ -4,7 +4,24 @@ var _express = _interopRequireDefault(require("express"));
 
 var _db = require("./config/db");
 
+var _index = _interopRequireDefault(require("../models/index"));
+
+var _uuid = require("uuid");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+var port = process.env.PORT || 4000;
+var Message = _index["default"].Message;
 
 _db.db.authenticate().then(function () {
   return console.log("connected to ".concat(process.env.NODE_ENV, " db"));
@@ -12,23 +29,246 @@ _db.db.authenticate().then(function () {
   return console.log("something went wrong connecting to the database: " + error);
 });
 
-var port = process.env.PORT || 4000;
-var app = (0, _express["default"])(); // app.get("/", (req, res) => {
-//   res.status(200).json({ message: "Welcome to Node.js & Express" });
-// });
-// app.get("/message/:id", (req, res) => {
-//   console.log(parseInt(req.params.id) + 5);
-//   res
-//     .status(200)
-//     .json({ message: `Yes hello this is message ${req.params.id}` });
-// });
-// Routes todo
+var app = (0, _express["default"])();
+app.use(_express["default"].json());
+app.use(_express["default"].urlencoded({
+  extended: true
+})); // Routes todo
 // Get all messages
-// Get single message by ID
-// Create a message
-// Update a message by ID
-// delete a message by ID
 
+app.get("/messages", /*#__PURE__*/function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(req, res) {
+    var messages;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.prev = 0;
+            _context.next = 3;
+            return Message.findAll();
+
+          case 3:
+            messages = _context.sent;
+            res.status(200).send(messages);
+            _context.next = 11;
+            break;
+
+          case 7:
+            _context.prev = 7;
+            _context.t0 = _context["catch"](0);
+            console.log(_context.t0);
+            res.status(404).json({
+              status: "failed",
+              error: "".concat(_context.t0)
+            });
+
+          case 11:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee, null, [[0, 7]]);
+  }));
+
+  return function (_x, _x2) {
+    return _ref.apply(this, arguments);
+  };
+}()); // Get single message by ID
+
+app.get("/messages/:id", /*#__PURE__*/function () {
+  var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(req, res) {
+    var id, message;
+    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            id = req.params.id;
+            _context2.prev = 1;
+            _context2.next = 4;
+            return Message.findAll({
+              where: {
+                id: id
+              }
+            });
+
+          case 4:
+            message = _context2.sent;
+            res.status(200).send(message);
+            _context2.next = 12;
+            break;
+
+          case 8:
+            _context2.prev = 8;
+            _context2.t0 = _context2["catch"](1);
+            console.log(_context2.t0);
+            res.status(404).json({
+              status: "failed",
+              error: "".concat(_context2.t0)
+            });
+
+          case 12:
+          case "end":
+            return _context2.stop();
+        }
+      }
+    }, _callee2, null, [[1, 8]]);
+  }));
+
+  return function (_x3, _x4) {
+    return _ref2.apply(this, arguments);
+  };
+}()); // Create a message
+
+app.post("/messages/", /*#__PURE__*/function () {
+  var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3(req, res) {
+    var content, id, message;
+    return regeneratorRuntime.wrap(function _callee3$(_context3) {
+      while (1) {
+        switch (_context3.prev = _context3.next) {
+          case 0:
+            content = req.body.content;
+            console.log(req.body);
+            id = (0, _uuid.v4)();
+            _context3.prev = 3;
+            _context3.next = 6;
+            return Message.create({
+              id: id,
+              content: content,
+              retrievedCounter: 0
+            });
+
+          case 6:
+            message = _context3.sent;
+            res.status(201).send(message);
+            _context3.next = 14;
+            break;
+
+          case 10:
+            _context3.prev = 10;
+            _context3.t0 = _context3["catch"](3);
+            console.log(_context3.t0);
+            res.status(400).json({
+              status: "failed",
+              error: "".concat(_context3.t0)
+            });
+
+          case 14:
+          case "end":
+            return _context3.stop();
+        }
+      }
+    }, _callee3, null, [[3, 10]]);
+  }));
+
+  return function (_x5, _x6) {
+    return _ref3.apply(this, arguments);
+  };
+}()); // Update a message by ID
+
+app.put("/messages/:id", /*#__PURE__*/function () {
+  var _ref4 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee4(req, res) {
+    var changes, id, message;
+    return regeneratorRuntime.wrap(function _callee4$(_context4) {
+      while (1) {
+        switch (_context4.prev = _context4.next) {
+          case 0:
+            changes = req.body;
+            id = req.params.id;
+            _context4.prev = 2;
+            _context4.next = 5;
+            return Message.update(_objectSpread({}, changes), {
+              where: {
+                id: id
+              },
+              returning: true
+            });
+
+          case 5:
+            message = _context4.sent;
+            res.status(202).send({
+              status: "success",
+              message: message[1][0]
+            });
+            _context4.next = 13;
+            break;
+
+          case 9:
+            _context4.prev = 9;
+            _context4.t0 = _context4["catch"](2);
+            console.log(_context4.t0);
+            res.status(400).json({
+              status: "failed",
+              error: "".concat(_context4.t0)
+            });
+
+          case 13:
+          case "end":
+            return _context4.stop();
+        }
+      }
+    }, _callee4, null, [[2, 9]]);
+  }));
+
+  return function (_x7, _x8) {
+    return _ref4.apply(this, arguments);
+  };
+}()); // delete a message by ID
+
+app["delete"]("/messages/:id", /*#__PURE__*/function () {
+  var _ref5 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee5(req, res) {
+    var id, message;
+    return regeneratorRuntime.wrap(function _callee5$(_context5) {
+      while (1) {
+        switch (_context5.prev = _context5.next) {
+          case 0:
+            id = req.params.id;
+            _context5.prev = 1;
+            _context5.next = 4;
+            return Message.destroy({
+              where: {
+                id: id
+              },
+              returning: true
+            });
+
+          case 4:
+            message = _context5.sent;
+
+            if (!(message === 0)) {
+              _context5.next = 7;
+              break;
+            }
+
+            throw new Error("Message could not be found");
+
+          case 7:
+            res.status(202).send({
+              status: "success"
+            });
+            _context5.next = 14;
+            break;
+
+          case 10:
+            _context5.prev = 10;
+            _context5.t0 = _context5["catch"](1);
+            console.log(_context5.t0);
+            res.status(400).json({
+              status: "failed",
+              error: "".concat(_context5.t0)
+            });
+
+          case 14:
+          case "end":
+            return _context5.stop();
+        }
+      }
+    }, _callee5, null, [[1, 10]]);
+  }));
+
+  return function (_x9, _x10) {
+    return _ref5.apply(this, arguments);
+  };
+}());
 app.listen(port, function () {
   console.log("App started on port ".concat(port));
 });
