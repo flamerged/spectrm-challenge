4068d452cbbf1fd0c0ae626e393568dd0af6fb90
Mersad Ajanovic <mersad.ajanovic@googlemail.com>
2021-02-11 14:53:24 +0100

    added message model

---
+++ migrations/20210211135100-create-message.js
@@ -0,0 +1,30 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Messages", {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      content: {
+        type: Sequelize.STRING(255),
+      },
+      retrievedCounter: {
+        type: Sequelize.BIGINT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Messages");
+  },
+};
